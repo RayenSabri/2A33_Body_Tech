28a9ed425cc2e1cc7262151698eaa1815e55ed69
adamhm123 <adam.benhajmabrouk@esprit.tn>
2023-04-28 14:18:23 +0100

    first commit

--- view/afficheA.php
+++ view/afficheA.php
@@ -1,49 +1,399 @@
-<?php
-	include_once 'C:\xampp\htdocs\body tech/Controller/adminC.php';
-	$adminC=new adminC();
-	$listeadmin=$adminC->afficheradmin(); 
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title>BodyTech - Acceuil</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<!--
+
+TemplateMo 546 Sixteen Clothing
+
+https://templatemo.com/tm-546-sixteen-clothing
+
+-->
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-sixteen.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+
+  </head>
+
+  <body>
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+
+    <!-- Header -->
+    <header class="">
+      <nav class="navbar navbar-expand-lg">
+        <div class="container">
+          <a class="navbar-brand" href="index.html"><h2>Body <em>Tech</em></h2></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.html">Acceuil
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li> 
+              <li class="nav-item">
+                <a class="nav-link" href="products.html">Nos produits</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.html">A propos</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="panier.html.html">panier</a>
+              </li>
+              
+              
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+
+    <!-- Page Content -->
+    <!-- Banner Starts Here -->
+    <div class="banner header-text">
+      <div class="owl-banner owl-carousel">
+        <div class="banner-item-01">
+          <div class="text-content">
+            <h4>Bienvenue</h4>
+            <h2>Prends soin de ton corps</h2>
+          </div>
+        </div>
+        <div class="banner-item-02">
+          <div class="text-content">
+            <h4>flash offres</h4>
+            <h2>Les meilleurs produits</h2>
+          </div>
+        </div>
+        <div class="banner-item-03">
+          <div class="text-content">
+            <h4>Regimes Ideales</h4>
+            <h2>Pour une meilleure santé</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Banner Ends Here -->
+
+    <div class="latest-products">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-heading">
+              <h2>Derniers Produits</h2>
+              <a href="products.html">Voir tous les produits <i class="fa fa-angle-right"></i></a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="panier.html.html"><img src="assets/images/product_01.jpg" alt=""></a>
+              <div class="down-content">
+                <a href="#"><h4>body tech EMS</h4></a>
+                <h6>199DT</h6>
+                <p>Le seul appareil du marché qui produit une sensation confortable et agréable tout en soumettant les muscles à un travail intense et profond..</p>
+                <ul class="stars">
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                </ul>
+                <span>Reviews (24)</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/product_02.jpg" alt=""></a>
+              <div class="down-content">
+                <a href="#"><h4>Body tech Shape</h4></a>
+                <h6>189 DT</h6>
+                <p>.</p>body tech Shape est un tout nouveau système d’entraînement qui utilise la technologie EMS et fait travailler simultanément différentes parties du corps, avec des résultats bien meilleurs que ceux de l’exercice classique.
+                <ul class="stars">
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                </ul>
+                <span>Reviews (21)</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/product_03.jpg" alt=""></a>
+              <div class="down-content">
+                <a href="#"><h4>body tech premium pack</h4></a>
+                <h6>169 DT</h6>
+                <p>Sixteen Clothing is free CSS template provided by TemplateMo.</p>
+                <ul class="stars">
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                </ul>
+                <span>Reviews (36)</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/product_04.jpg" alt=""></a>
+              <div class="down-content">
+                <a href="#"><h4>Body Tech multivitamins</h4></a>
+                <h6>59 DT</h6>
+                <p>Une meilleure récupération. Après un effort intense, le corps a besoin de repos..</p>
+                <ul class="stars">
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                </ul>
+                <span>Reviews (48)</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/product_05.jpg" alt=""></a>
+              <div class="down-content">
+                <a href="#"><h4>Body Tech calories</h4></a>
+                <h6>250 DT</h6>
+                <p>c'est une machine smart qui permet a l'utilisateur de calculer les  calories.</p>
+                <ul class="stars">
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                </ul>
+                <span>Reviews (16)</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/product_06.jpg" alt=""></a>
+              <div class="down-content">
+                <a href="#"><h4>Body Tech Watch</h4></a>
+                <h6>69 DT</h6>
+                <p>c'est une montre smart qui permet au utilisateur de calculer les battements de coeur.</p>
+                <ul class="stars">
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                  <li><i class="fa fa-star"></i></li>
+                </ul>
+                <span>Reviews (32)</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="best-features">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-heading">
+              <h2>About Sixteen Clothing</h2>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="left-content">
+              <h4>Looking for the best products?</h4>
+              <p><a rel="nofollow" href="https://templatemo.com/tm-546-sixteen-clothing" target="_parent">This template</a> is free to use for your business websites. However, you have no permission to redistribute the downloadable ZIP file on any template collection website. <a rel="nofollow" href="https://templatemo.com/contact">Contact us</a> for more info.</p>
+              <ul class="featured-list">
+                <li><a href="#">Lorem ipsum dolor sit amet</a></li>
+                <li><a href="#">Consectetur an adipisicing elit</a></li>
+                <li><a href="#">It aquecorporis nulla aspernatur</a></li>
+                <li><a href="#">Corporis, omnis doloremque</a></li>
+                <li><a href="#">Non cum id reprehenderit</a></li>
+              </ul>
+              <a href="about.html" class="filled-button">Read More</a>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="right-image">
+              <img src="assets/images/feature-image.jpg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    
+
+
+    <div>
+
+      <!--CRUD-->
+
+      <?php
+
+include_once 'C:\xampp\htdocs\body tech/Controller/adminC.php';
+$adminC=new adminC();
+$listeadmin=$adminC->afficheradmin(); 
 ?>
 <html>
-	<head></head>
-	<body>
-	    <button><a href="addA.php">Ajouter un admin</a></button>
-		<center><h1>Liste des admin</h1></center>
-		<table border="1" align="center">
-			<tr>
-				<th>cin</th>
-				<th>nom</th>
-				<th>prenom</th>
-				<th>salaire</th>
-				<th>email</th>
-				<th>tel</th>
-				<th>password</th>
-				<th>Modifier</th>
-				<th>Supprimer</th>
-			</tr>
-			<?php
-				foreach($listeadmin as $admin){
-			?>
-			<tr>
-				<td><?php echo $admin['cin']; ?></td>
-				<td><?php echo $admin['nom']; ?></td>
-				<td><?php echo $admin['prenom']; ?></td>
-				<td><?php echo $admin['salaire']; ?></td>
-				<td><?php echo $admin['email']; ?></td>
-				<td><?php echo $admin['tel']; ?></td>
-				<td><?php echo $admin['password']; ?></td>
-				<td>
-					<form method="POST" action="modifierA.php">
-						<input type="submit" name="Modifier" value="Modifier">
-						<input type="hidden" value=<?PHP echo $admin['cin']; ?> name="cin">
-					</form>
-				</td>
-				<td>
-					<a href="supprimerA.php?cin=<?php echo $admin['cin']; ?>">Supprimer</a>
-				</td>
-			</tr>
-			<?php
-				}
-			?>
-		</table>
-	</body>
+<head></head>
+<body>
+    <button><a href="addA.php">Ajouter un admin</a></button>
+    <h1>Recherche d'utilisateur</h1>
+    <form method="post">
+        <label for="username">Nom d'utilisateur :</label>
+        <input type="text" id="username" name="nom" required>
+        <button type="submit">Rechercher</button>
+    </form>
+    <?php
+    // Traitement du formulaire de recherche
+try {
+  $conn = new PDO("mysql:host=localhost;dbname=gestion des utilisateurs", 'root', '');
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   } catch(PDOException $e) {
+  echo "Erreur de connexion à la base de données: " . $e->getMessage();
+   }
+      if (isset($_POST['nom'])) {
+          $nom = $_POST['nom'];
+  $users = searchUsersByName($conn, $nom);
+          if (count($users) > 0) {
+              echo '<h2>Résultats de la recherche :</h2>';
+              echo '<ul>';
+              foreach ($users as $user) {
+                  echo '<li>' . $user['nom'] . ' ' . $user['prenom'] . ' ' . $user['type']. ' ' . $user['email']. ' ' . $user['tel']. '</li>';
+              }
+              echo '</ul>';
+          } else {
+              echo '<p>Aucun utilisateur trouvé.</p>';
+          }
+      }
+  
+  function searchUsersByName($conn, $nom) {
+          $stmt = $conn->prepare("SELECT * FROM admin WHERE nom LIKE :nom or prenom LIKE :nom");
+          $stmt->bindValue(':nom', '%'.$nom.'%');
+          $stmt->execute();
+          return $stmt->fetchAll(PDO::FETCH_ASSOC);
+      }
+   // Fermeture de la connexion à la base de données
+      $conn = null;
+  
+    ?>
+  <center><h1>Liste des admin</h1></center>
+  <table border="1" align="center">
+    <tr>
+      <th>cin</th>
+      <th>nom</th>
+      <th>prenom</th>
+      <th>salaire</th>
+      <th>email</th>
+      <th>tel</th>
+      <th>password</th>
+      <th>Modifier</th>
+      <th>Supprimer</th>
+    </tr>
+    <?php
+      foreach($listeadmin as $admin){
+    ?>
+    <tr>
+      <td><?php echo $admin['cin']; ?></td>
+      <td><?php echo $admin['nom']; ?></td>
+      <td><?php echo $admin['prenom']; ?></td>
+      <td><?php echo $admin['type']; ?></td>
+      <td><?php echo $admin['email']; ?></td>
+      <td><?php echo $admin['tel']; ?></td>
+      <td><?php echo $admin['password']; ?></td>
+      <td>
+        <form method="POST" action="modifierA.php">
+          <input type="submit" name="Modifier" value="Modifier">
+          <input type="hidden" value=<?PHP echo $admin['cin']; ?> name="cin">
+        </form>
+      </td>
+      <td>
+        <a href="supprimerA.php?cin=<?php echo $admin['cin']; ?>">Supprimer</a>
+      </td>
+    </tr>
+    <?php
+      }
+    ?>
+  </table>
+</body>
+</html>
+
+    
+
+
+    </div>
+
+    
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="inner-content">
+              <p>Copyright &copy; 2020 Sixteen Clothing Co., Ltd.
+            
+            - Design: <a rel="nofollow noopener" href="https://templatemo.com" target="_blank">TemplateMo</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+    <!-- Additional Scripts -->
+    <script src="assets/js/custom.js"></script>
+    <script src="assets/js/owl.js"></script>
+    <script src="assets/js/slick.js"></script>
+    <script src="assets/js/isotope.js"></script>
+    <script src="assets/js/accordions.js"></script>
+
+
+    <script language = "text/Javascript"> 
+      cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
+      function clearField(t){                   //declaring the array outside of the
+      if(! cleared[t.id]){                      // function makes it static and global
+          cleared[t.id] = 1;  // you could use true and false, but that's more typing
+          t.value='';         // with more chance of typos
+          t.style.color='#fff';
+          }
+      }
+    </script>
+
+
+  </body>
+
 </html>
